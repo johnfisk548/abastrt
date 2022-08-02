b6fd00045ba9b39a47ab5be7e27caf6345dfa922
John Fisk <70089538+johnfisk548@users.noreply.github.com>
2022-08-02 08:11:52 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -158,7 +158,7 @@ def get_readable_message():
                 msg += f" | <b>Time: </b>{get_readable_time(download.torrent_info().seeding_time)}"
             else:
                 msg += f"\n<b>Size: </b>{download.size()}"
-            msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            msg += f"\n<b>To Cancel:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += "\n_______________________________"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
